bb945cb1ea707b8c6d1a0afb46608e72fcf38d7b
wesinator <5124946+wesinator@users.noreply.github.com>
2024-06-28 01:57:56 -0400

    handle reference footnotes
    # NO_TRAIN

--- get_iana_tls_alpn_protocol_ids.js
+++ get_iana_tls_alpn_protocol_ids.js
@@ -3,12 +3,20 @@
 var rr_tbl = document.getElementById("table-alpn-protocol-ids");
 
 var records = []
-for (var record of rr_tbl.children[1].children) { 
-        records.push({
-            protocol: record.children[0].textContent,
-            identification_sequence: record.children[1].textContent,
-			reference: record.children[2].textContent.replaceAll("][", ", ").replaceAll('[', '').replaceAll(']', '').trim(),
-        });
+for (var record of rr_tbl.children[1].children) {
+    reference = record.children[2];
+    obj = {
+        protocol: record.children[0].textContent,
+        identification_sequence: record.children[1].textContent,
+        reference: reference.textContent.replaceAll("][", ", ").replaceAll('[', '').replaceAll(']', '').trim(),
+    }
+
+    // check for note field in column 3
+    reference_firstLink = record.children[2].children[0].attributes[0].nodeValue;
+    if (reference_firstLink.startsWith('#'))
+        obj.note = document.getElementById(reference_firstLink.slice(1)).parentElement.nextElementSibling.innerText.trim();
+
+    records.push(obj);
 }
 
 console.log(JSON.stringify(records, null, 4));
